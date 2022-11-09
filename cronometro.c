ce9d09aeb2c3eedbc5779cbcf288e1f0229dab8c
gl-schneck <99268789+gl-schneck@users.noreply.github.com>
2022-11-09 09:56:01 -0300

    Update cronometro.c

--- cronometro.c
+++ cronometro.c
@@ -5,12 +5,12 @@ Projeto da Emotion Now em conjunto com a Flow
 */
 
 //Bibliotecas
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#include<windows.h>
-#include<math.h>
-#include<conio.h>
+#include<stdio.h> //Biblioteca necessárioa para input e output
+#include<time.h> //Biblioteca necessária para parar o código por 1seg
+#include<stdlib.h> //Biblioteca necessária para obter mais funções necessárias para o código
+#include<windows.h> //Biblioteca necessária para limpar o console
+#include<math.h> //Biblioteca necessária para fazer operações matemáticas no código
+#include<conio.h> //Biblioteca necessária para detectar input de teclado sem parar o código
 
 int main()
 {
@@ -18,10 +18,12 @@ int main()
     int sec = 0;
     int min = 0;
     int hour = 0;
+    int data = 0;
     char inp;
 
-    scanf("%c", &inp); //Detecta se alguma tecla foi pressionada para iniciar o cronômetro
-    if(inp)
+    printf("Pressione ENTER para comecar\n\nQuando comecar, pressione qualquer tecla para parar");
+    scanf("%c", &inp);
+    if(inp) //Detecta se alguma tecla foi pressionada para iniciar o cronômetro
     {
         Sleep(10);
         while (1)
@@ -41,9 +43,11 @@ int main()
                         hour++;
                     }
                 }
-            
+
+                data = 194927035+rand() % (1000000000-194927035); //Gerador de dados aleatórios, pois não temos acesso aos reais ainda
+
                 system("cls");
-                printf("%d:%d:%d -- %d\n", hour, min, sec, 194927035+rand() % (1000000000-194927035)); //Imprime h:m:s -- Dado do sensor
+                printf("%d:%d:%d -- %d\n", hour, min, sec, data); //Imprime h:m:s -- Dado do sensor
                 sec++;
                 Sleep(1000); //Intervalo de 1s entre coletas de dados
             }
